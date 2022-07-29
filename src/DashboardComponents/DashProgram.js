4af5d68918945f17466e6386a275071da9a6398a
Casaemiro <silasmagho@gmail.com>
2022-07-29 08:34:28 +0100

    dashboard

--- src/DashboardComponents/DashProgram.js
+++ src/DashboardComponents/DashProgram.js
@@ -4,13 +4,13 @@ import Created from './created';
 const Program = () => {
     return ( 
         <div className="program">
-            <div className='dashcreatedevents col-4'>
+            <div className='dashcreatedevents col-md-4'>
                 <div className='createdEvents'>Trainings</div>
                 <Created />
                 <Created />
                 <Created />
             </div>
-            <div className='dashcreateevents col-6 p-md-5 p-sm-3 p-1' >
+            <div className='dashcreateevents col-md-6 p-md-5 p-sm-3 p-1' >
                 <div className='createdEvents pb-5'>Add training</div>
                 <div className="form-floating mb-3">
                     <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
