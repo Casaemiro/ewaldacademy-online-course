edde717052b4d71193f24e7cbb536cbb43860599
Casaemiro <silasmagho@gmail.com>
2022-07-30 10:07:15 +0100

    added firebase db

--- src/App.js
+++ src/App.js
@@ -12,22 +12,57 @@ import ContactusPage from './Pages/ContactusPage';
 import Events from './Pages/Events';
 import SociaPop from './components/socialpop';
 import Dashboard from './Pages/dashboard';
-
+import { useState, useEffect } from 'react';
+import { db } from './firebaseconfig'
+import { collection, getDocs } from 'firebase/firestore'
 function App() {
+
+
+  const [posts, setPosts] = useState([
+    { Caption: "", description: "", hashtag: "" }
+  ])
+  const [program, setProgram] = useState([
+    { name: "", description: "", cost: 0 }
+  ])
+
+
+  const postsCollectionRef = collection(db, 'posts')
+  const programCollectionRef = collection(db, 'programs')
+  useEffect(() => {
+
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      const data2 = await getDocs(programCollectionRef);
+      console.log(data2)
+
+      setProgram(data2.docs.map((post) => ({ ...post.data() })))
+      setPosts(data.docs.map((post) => ({ ...post.data() })))
+
+
+    }
+    getPosts()
+
+  }, [])
+
+  console.log(posts)
+  console.log(program)
+
+
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <SociaPop />
         <Routes>
-        <Route index element={<HomePage/>}/>
-        <Route path="/contactus" element={<ContactusPage />}/>
-        <Route path='/Abt' element={<AboutPage/>}/>
-        <Route path='/musiccourse' element={<MusicBasics/>}/>
-        <Route path='/admission' element={<Admission />} />
-        <Route path='/courselist' element={<CourseList />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+          <Route index element={<HomePage />} />
+          <Route path="/contactus" element={<ContactusPage />} />
+          <Route path='/Abt' element={<AboutPage />} />
+          <Route path='/musiccourse' element={<MusicBasics />} />
+          <Route path='/admission' element={<Admission programs = {program}/>} />
+          <Route path='/courselist' element={<CourseList />} />
+          <Route path='/events' element={<Events post={posts} />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
 
         <Contactus />
